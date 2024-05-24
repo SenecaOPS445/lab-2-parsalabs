9f7d045fd28d88e058e44e09678ef14d105916ea
“Parsa <pbalari@myseneca.ca>
2024-05-24 16:15:38 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+#Author: Parsa Balari
+#Author ID: pbalari
+#Date Created: 2024/05/24
+
+timer = 10
+
+while timer > 0:
+    print(timer)
+    timer -= 1
+
+print("blast off!")
